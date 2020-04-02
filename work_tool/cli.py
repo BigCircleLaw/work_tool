793899f76897838c14ce81a7001ee9ba9e5bd84f
Big Circle <794557226@qq.com>
2020-04-02 11:18:11 +0800

    添加两个文件。

---
+++ work_tool/cli.py
@@ -0,0 +1,8 @@
+'''
+@Author: your name
+@Date: 2020-04-02 11:11:47
+@LastEditTime: 2020-04-02 11:11:47
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \wonderbits_tool\work_tool\cli.py
+'''
---
+++ work_tool/upload.py
@@ -0,0 +1,8 @@
+'''
+@Author: your name
+@Date: 2020-04-02 11:12:04
+@LastEditTime: 2020-04-02 11:12:04
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \wonderbits_tool\work_tool\upload.py
+'''
